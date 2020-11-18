439413890a6c9b76277141655271e0dcfea9884d
Brijesh Kotaria <65158427+brijesh-989@users.noreply.github.com>
2020-11-18 11:49:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text,StyleSheet ,Dimensions} from 'react-native';
+
+import Home from './app/home'
+import Splash from './app/splash'
+import App from './app/navigate'
+import SelectStatem from './app/mpc/statesm'
+import SelectStateb from './app/bpc/statesb'
+//import Apps from './app/samp'
+
+export default function componentName() {
+  return (
+    <App/>
+  );
+}
+
